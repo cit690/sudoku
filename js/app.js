6830f7ca8205d620f11c15a3a5aeaacb9c878ed4
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-02-13 14:58:02 -0500

    setting up renderarrays

--- js/app.js
+++ js/app.js
@@ -1,29 +1,30 @@
 /*-------------------------------- Constants --------------------------------*/
 const boxArray = [
-  [1, 2, 3, 10, 11, 12, 19, 20, 21],
-  [4, 5, 6, 13, 14, 15, 22, 23, 24],
-  [7, 8, 9, 16, 17, 18, 25, 26, 27],
-  [28, 29, 30, 37, 38, 39, 46, 47, 48],
-  [31, 32, 33, 40, 41, 42, 49, 50, 51],
-  [34, 35, 36, 43, 44, 45, 52, 53, 54],
-  [55, 56, 57, 64, 65, 66, 73, 74, 75],
-  [58, 59, 60, 67, 68, 69, 76, 77, 78],
-  [61, 62, 63, 70, 71, 72, 79, 80, 81],
+  [0, 1, 2, 9, 10, 11, 18, 19, 20],
+  [3, 4, 5, 12, 13, 14, 21, 22, 23],
+  [6, 7, 8, 15, 16, 17, 24, 25, 26],
+  [27, 28, 29, 36, 37, 38, 45, 46, 47],
+  [30, 31, 32, 39, 40, 41, 48, 49, 50],
+  [33, 34, 35, 42, 43, 44, 51, 52, 53],
+  [54, 55, 56, 63, 64, 65, 72, 73, 74],
+  [57, 58, 59, 66, 67, 68, 75, 76, 77],
+  [60, 61, 62, 69, 70, 71, 78, 79, 80],
 ]
 
 const rowsArray = [
-  [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  [10, 11, 12, 13, 14, 15, 16, 17, 18],
-  [19, 20, 21, 22, 23, 24, 25, 26, 27],
-  [28, 29, 30, 31, 32, 33, 34, 35, 36],
-  [37, 38, 39, 40, 41, 42, 43, 44, 45],
-  [46, 47, 48, 49, 50, 51, 52, 53, 54],
-  [55, 56, 57, 58, 59, 60, 61, 62, 63],
-  [64, 65, 66, 67, 68, 69, 70, 71, 72],
-  [73, 74, 75, 76, 77, 78, 79, 80, 81],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  [9, 10, 11, 12, 13, 14, 15, 16, 17],
+  [18, 19, 20, 21, 22, 23, 24, 25, 26],
+  [27, 28, 29, 30, 31, 32, 33, 34, 35],
+  [36, 37, 38, 39, 40, 41, 42, 43, 44],
+  [45, 46, 47, 48, 49, 50, 51, 52, 53],
+  [54, 55, 56, 57, 58, 59, 60, 61, 62],
+  [63, 64, 65, 66, 67, 68, 69, 70, 71],
+  [72, 73, 74, 75, 76, 77, 78, 79, 80],
 ]
 
 const columnsArray = [
+  [0, 9, 18, 27, 36, 45, 54, 63, 72],
   [1, 10, 19, 28, 37, 46, 55, 64, 73],
   [2, 11, 20, 29, 38, 47, 56, 65, 74],
   [3, 12, 21, 30, 39, 48, 57, 66, 75],
@@ -32,7 +33,6 @@ const columnsArray = [
   [6, 15, 24, 33, 42, 51, 60, 69, 78],
   [7, 16, 25, 34, 43, 52, 61, 70, 79],
   [8, 17, 26, 35, 44, 53, 62, 71, 80],
-  [9, 18, 27, 36, 45, 54, 63, 72, 81],
 ]
 
 const allSolutions = boxArray.concat(rowsArray, columnsArray)
@@ -42,11 +42,12 @@ const allSolutions = boxArray.concat(rowsArray, columnsArray)
 
 let player
 let time
+let click
 
 /*------------------------ Cached Element References ------------------------*/
 const playerBoard = document.querySelector('.board')
 console.log(playerBoard)
-for(let i=1; i<=81; i++) {
+for(let i=0; i<=80; i++) {
   const newDiv = document.createElement('div')
   newDiv.className = 'tile'
   newDiv.id = 'sq' + i
@@ -63,9 +64,6 @@ const numBtnEls = document.querySelectorAll('.select')
 console.log(numBtnEls)
 
 /*----------------------------- Event Listeners -----------------------------*/
-//I want a to be able to click on a tile, have that tile listen for a click from one of the number buttons, and have the value of the number button be displayed on the selected tile.
-
-
 
 playerBoard.addEventListener('click', function(evt){
   let choice = parseInt(evt.target.id.replace('sq', ''))
@@ -80,49 +78,65 @@ playerBoard.addEventListener('click', function(evt){
     numberSelectEl.removeEventListener('click', storeValue)
   }
 
+  renderArrays()
 })
-
-
-  // I need the input to remain the assigned value after I select a new tile, without changing to the newest assigned value.
-
-
-
-
 
 /*-------------------------------- Functions --------------------------------*/
 
 
 
 function init(){
-  blankBoard = [],
+  blankBoard = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
   possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   player = 1
+  click = 0
+  win = null
 
-  
+  // render()
 }
 init()
 
+function render(){
 
-// function render(){
-  
-  
-  
-// }
+}
+
 //I want the board within each array to accept 1 integer between 1 and 9, without any of the integers repeating.
 
-// function renderArrays(){
-//   for (let i = 0; i <= possibleValues.length; i++) {
-//     const a = allSolutions[i][0]
-//     const b = allSolutions[i][1]
-//     const c = allSolutions[i][2]
-//     const d = allSolutions[i][3]
-//     const e = allSolutions[i][4]
-//     const f = allSolutions[i][5]
-//     const g = allSolutions[i][6]
-//     const h = allSolutions[i][7]
-//     const k = allSolutions[i][8]
-//   }
-// }
+function renderArrays(){
+  for (let i = 0; i <= possibleValues.length; i++) {
+    const a = allSolutions[i][0]
+    const b = allSolutions[i][1]
+    const c = allSolutions[i][2]
+    const d = allSolutions[i][3]
+    const e = allSolutions[i][4]
+    const f = allSolutions[i][5]
+    const g = allSolutions[i][6]
+    const h = allSolutions[i][7]
+    const j = allSolutions[i][8]
+
+    if(playerBoard[a]+playerBoard[b]+playerBoard[c]+playerBoard[d]+playerBoard[e]+playerBoard[f]+playerBoard[g]+playerBoard[h]+playerBoard[j] === true){
+      console.log('win')
+    }
+
+  }
+}
+
+function evalNum()  {
+  checkIfNumIsAlreadyInRow()
+  checkIfNumIsAlreadyInColumn()
+  checkIfNumIsAlreadyInGrid()
+}
+
+function checkIfNumIsAlreadyInRow() {
+
+}
+
+function checkIfNumIsAlreadyInColumn() {
+
+}
+
+function checkIfNumIsAlreadyInGrid() {
+
 
 
     // [null, null, null, null, null, null, null, null, null],
